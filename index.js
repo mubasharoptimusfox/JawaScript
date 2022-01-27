ac46c8c7e8848d416e6abac579c2c2277728bdc1
Mohamad Shahmeer <Mohammad.Shahmeer@optimusfox.com>
2022-01-27 16:56:25 +0500

    Start Work

---
+++ index.js
@@ -0,0 +1,10 @@
+var myName = 'Mubashar';
+
+var myName = null;
+// console.log(myName);
+// console.log(typeof(myName));
+console.log(NaN === NaN);
+console.log(Number.NaN === NaN);
+console.log(isNaN(NaN));
+console.log(Number.isNaN(NaN));
+console.log(5+30);
